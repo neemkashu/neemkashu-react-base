f1bf546d81bd129a47d5b3b2812d9baabbaf8419
neemkashu <sparjinkust@mail.ru>
2023-06-23 01:14:42 +0300

    feat: add skeleton links to layout

--- src/app/Layouts/Header/Header.tsx
+++ src/app/Layouts/Header/Header.tsx
@@ -1,11 +1,20 @@
 import { FC } from "react";
 import styles from "./style.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
+const BACKET_ALT = "перейти к корзине";
+const LINKS = {
+  basket: "./basket",
+};
 
 export const Header: FC = () => {
   return (
     <header className={styles.header}>
       <h1>Билетопоиск</h1>
-      <a>Корзина</a>
+      <Link href={LINKS.basket}>
+        <Image src={"/basket.svg"} alt={BACKET_ALT} width={32} height={32} />
+      </Link>
     </header>
   );
 };
