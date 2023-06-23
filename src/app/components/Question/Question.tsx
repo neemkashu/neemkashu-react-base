764be3cd82782cb16872537fa73cef77cfde70c6
neemkashu <sparjinkust@mail.ru>
2023-06-23 07:23:54 +0300

    feat: add qa button icon

--- src/app/components/Question/Question.tsx
+++ src/app/components/Question/Question.tsx
@@ -1,12 +1,17 @@
 import { FC } from "react";
 import styles from "./style.module.css";
+import Image from "next/image";
 import { QuestionProps } from "../../utils/types";
+
+const ARROW_ALT = "посмотреть ответ";
 
 export const Question: FC<QuestionProps> = ({ question, answer }) => {
   return (
     <div className={styles.question}>
       <p>{question}</p>
-      <button>v</button>
+      <button className={styles.button}>
+        <Image src={"/arrow.svg"} alt={ARROW_ALT} width={32} height={32} />
+      </button>
     </div>
   );
 };
