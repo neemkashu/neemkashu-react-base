c3a41ea5cede397e9c940112bbd7e6cad601a307
neemkashu <sparjinkust@mail.ru>
2023-06-24 12:42:46 +0300

    feat: add buttons to filter fields

--- src/app/components/Filters/Filter/Filter.tsx
+++ src/app/components/Filters/Filter/Filter.tsx
@@ -1,12 +1,16 @@
+"use client";
+
 import { FC } from "react";
 import styles from "./style.module.css";
 import classNames from "classnames";
+import { DropButton } from "../../DropButton/DropButton";
 
 type FilterProps = {
   name: string;
+  isDropType?: boolean;
 };
 
-export const Filter: FC<FilterProps> = ({ name }) => {
+export const Filter: FC<FilterProps> = ({ name, isDropType }) => {
   return (
     <div className={styles.filter}>
       <label className={styles.label} htmlFor={name}>
@@ -17,6 +21,11 @@ export const Filter: FC<FilterProps> = ({ name }) => {
         id={name}
         type="text"
       />
+      {isDropType ? (
+        <div className={styles.relative}>
+          <DropButton handler={() => {}} size={20} />
+        </div>
+      ) : null}
     </div>
   );
 };
--- src/app/components/Filters/FilterList.tsx
+++ src/app/components/Filters/FilterList.tsx
@@ -2,14 +2,18 @@ import { FC } from "react";
 import styles from "./style.module.css";
 import { Filter } from "./Filter/Filter";
 
-const FilterConfig = ["Название", "Жанр", "Кинотеатр"];
+const FilterConfig = [
+  { name: "Название", isDropDown: false },
+  { name: "Жанр", isDropDown: true },
+  { name: "Кинотеатр", isDropDown: true },
+];
 
 export const FilterList: FC = () => {
   return (
     <div className={styles.filters}>
       <h2>Фильтры поиска</h2>
-      {FilterConfig.map((filter) => (
-        <Filter key={filter} name={filter} />
+      {FilterConfig.map(({ name, isDropDown }) => (
+        <Filter key={name} name={name} isDropType={isDropDown} />
       ))}
     </div>
   );
