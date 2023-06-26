b123e59ce38ac5c65498c20a8fd56ba2ce42e23f
neemkashu <sparjinkust@mail.ru>
2023-06-26 11:34:42 +0300

    refactor: add styles to edge cases

--- src/app/components/FilmList/FilmList.tsx
+++ src/app/components/FilmList/FilmList.tsx
@@ -3,12 +3,14 @@
 import { FC } from "react";
 import { FilmCard } from "../FilmCardShort/FilmCardShort";
 import { useFilteredFilms } from "../Filters/hooks/hooks";
+import styles from "./style.module.css";
 
 export const FilmList: FC = () => {
   const { data, isLoading } = useFilteredFilms();
 
-  if (isLoading) return <span>Loading</span>;
-  if (!data) return <span>No film was found</span>;
+  if (isLoading) return <span className={styles.empty}>Loading</span>;
+  if (!data || data.length === 0)
+    return <span className={styles.empty}>No film was found</span>;
 
   return (
     <>
--- src/app/components/Filters/SearchFilter/SearchFilter.tsx
+++ src/app/components/Filters/SearchFilter/SearchFilter.tsx
@@ -1,16 +1,12 @@
 import { FC } from "react";
-import { useAppDispatch, useAppSelector } from "../../../../redux/hooks";
+import { useAppDispatch } from "../../../../redux/hooks";
 import { updateSearchText } from "../../../../redux/search/searchSlice";
 import { TypeFilter } from "../UI/TypeFilter/TypeFilter";
-import { selectSearchText } from "../../../../redux/selectors";
 
 const SearchConfig = { name: "Название" };
 
 export const SearchFilter: FC = () => {
-  const searchQuery = useAppSelector(selectSearchText);
   const dispatch = useAppDispatch();
-  console.log("searchQuery", searchQuery);
-
   return (
     <TypeFilter
       name={SearchConfig.name}
