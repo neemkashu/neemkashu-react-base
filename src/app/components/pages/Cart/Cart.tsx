1fa004ae49ee7a97ee3456772da095287930d605
neemkashu <sparjinkust@mail.ru>
2023-06-27 02:04:55 +0300

    feat: add unique state for empty cart

--- src/app/components/pages/Cart/Cart.tsx
+++ src/app/components/pages/Cart/Cart.tsx
@@ -10,24 +10,30 @@ export const Cart: FC = () => {
   const films = useCartFilms();
   return (
     <div className={styles.cart}>
-      <div className={styles.list}>
-        {films
-          .filter(({ film }) => film)
-          .map(({ amount, film }) => {
-            if (!film) return null;
-            const { id, title, posterUrl, genre } = film;
-            return (
-              <FilmCardShort
-                key={id}
-                id={id}
-                title={title}
-                posterUrl={posterUrl}
-                genre={genre}
-              />
-            );
-          })}
-      </div>
-      <TotalCart />
+      {films?.length ? (
+        <>
+          <div className={styles.list}>
+            {films
+              .filter(({ film }) => film)
+              .map(({ amount, film }) => {
+                if (!film) return null;
+                const { id, title, posterUrl, genre } = film;
+                return (
+                  <FilmCardShort
+                    key={id}
+                    id={id}
+                    title={title}
+                    posterUrl={posterUrl}
+                    genre={genre}
+                  />
+                );
+              })}
+          </div>
+          <TotalCart />
+        </>
+      ) : (
+        <p className={styles.empty}>В корзине пока пусто</p>
+      )}
     </div>
   );
 };
